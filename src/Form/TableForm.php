f06ed7a689eaedbc6a6dafe60d08d095e394053e
Vitalik Prokopenko <vitalik00pr@gmail.com>
2022-05-23 15:26:58 +0300

    Add table form, create tables, some styles

--- src/Form/TableForm.php
+++ src/Form/TableForm.php
@@ -4,6 +4,8 @@ namespace Drupal\module4\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateinterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\MessageCommand;
 
 /**
  * Main class for form table.
@@ -18,11 +20,112 @@ class TableForm extends FormBase {
   }
 
   /**
+   * Contain counts rows in tables.
+   *
+   * @var array
+   *   Array with counts.
+   */
+  private $rows = [3, 2, 5];
+
+  /**
+   * Contain count tables on page.
+   *
+   * @var array
+   *   Count tables.
+   */
+  private $countTable = 3;
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $form['add-table'] = [
+      '#type' => 'button',
+      '#value' => $this->t('Add table'),
+      '#ajax' => [
+        'event' => 'click',
+        'callback' => '::addTable',
+      ],
+    ];
+
+    $table_headers = [
+      $this->t('Year'),
+      $this->t('Jan'),
+      $this->t('Feb'),
+      $this->t('Mar'),
+      $this->t('Q1'),
+      $this->t('Apr'),
+      $this->t('May'),
+      $this->t('Jun'),
+      $this->t('Q2'),
+      $this->t('Jul'),
+      $this->t('Aug'),
+      $this->t('Sep'),
+      $this->t('Q3'),
+      $this->t('Oct'),
+      $this->t('Nov'),
+      $this->t('Dec'),
+      $this->t('Q4'),
+      $this->t('YTD'),
+    ];
+
+    for ($table = 0; $table < $this->countTable; $table++) {
+
+      $form["table-$table"] = [
+        '#type' => 'table',
+        '#header' => $table_headers,
+      ];
+
+      for ($i = 0; $i < $this->rows[$table]; $i++) {
+        foreach ($table_headers as $header) {
+          if ($header == 'Year') {
+            $form["table-$table"][$i]["$header"] = [
+              '#type' => 'html_tag',
+              '#tag' => 'div',
+              '#value' => date('Y') - $i,
+            ];
+          }
+          elseif (in_array($header, ['Q1', 'Q2', 'Q3', 'Q4', 'YTD'])) {
+            $form["table-$table"][$i]["$header"] = [
+              '#type' => 'html_tag',
+              '#tag' => 'div',
+              '#value' => 0,
+            ];
+          }
+          else {
+            $form["table-$table"][$i]["$header"] = [
+              '#type' => 'number',
+            ];
+          }
+        }
+      }
+
+      $form["addRow_$table"] = [
+        '#type' => 'button',
+        '#value' => $this->t("Add row"),
+        '#ajax' => [
+          'event' => 'click',
+          'callback' => '::addRow',
+        ],
+      ];
+    }
+
+    $form["#attached"]["library"][] = "module4/module4";
     return $form;
+  }
+
+  /**
+   * Callback function for add row button.
+   */
+  public function addRow(array &$form, FormStateInterface $form_state) {
+    $i = $form_state->getTriggeringElement()['#name'];
+    $this->rows[$i]++;
+    $form_state->setRebuild();
+
+    $response = new AjaxResponse();
+    $response->addCommand(new MessageCommand("$i"));
+    return $response;
   }
 
   /**
