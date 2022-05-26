e0c1db170a79f82f97efa5c0218010f3afae036f
Vitalik Prokopenko <vitalik00pr@gmail.com>
2022-05-26 13:49:01 +0300

    Make second validation and fix first

--- src/Form/TableForm.php
+++ src/Form/TableForm.php
@@ -4,7 +4,6 @@ namespace Drupal\module4\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateinterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Main class for form table.
@@ -14,8 +13,8 @@ class TableForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
-    return 'form_table';
+  public function getFormId(): string {
+    return "form_table";
   }
 
   /**
@@ -24,69 +23,72 @@ class TableForm extends FormBase {
    * @var array
    *   Array with counts.
    */
-  protected $rows = [1];
+  protected array $rows = [1];
 
   /**
    * Contain count tables on page.
    *
-   * @var array
+   * @var int
    *   Count tables.
    */
-  protected $countTable = 1;
+  protected int $countTable = 1;
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state): array {
 
     $table_headers = [
-      $this->t('Year'),
-      $this->t('Jan'),
-      $this->t('Feb'),
-      $this->t('Mar'),
-      $this->t('Q1'),
-      $this->t('Apr'),
-      $this->t('May'),
-      $this->t('Jun'),
-      $this->t('Q2'),
-      $this->t('Jul'),
-      $this->t('Aug'),
-      $this->t('Sep'),
-      $this->t('Q3'),
-      $this->t('Oct'),
-      $this->t('Nov'),
-      $this->t('Dec'),
-      $this->t('Q4'),
-      $this->t('YTD'),
+      $this->t("Year"),
+      $this->t("Jan"),
+      $this->t("Feb"),
+      $this->t("Mar"),
+      $this->t("Q1"),
+      $this->t("Apr"),
+      $this->t("May"),
+      $this->t("Jun"),
+      $this->t("Q2"),
+      $this->t("Jul"),
+      $this->t("Aug"),
+      $this->t("Sep"),
+      $this->t("Q3"),
+      $this->t("Oct"),
+      $this->t("Nov"),
+      $this->t("Dec"),
+      $this->t("Q4"),
+      $this->t("YTD"),
     ];
+
+    $form["#prefix"] = "<div id='form-wrapper'>";
+    $form["#suffix"] = "</div>";
 
     for ($table = 0; $table < $this->countTable; $table++) {
 
       $form["add-row-$table"] = [
-        '#type' => 'submit',
-        '#value' => $this->t("Add year"),
-        '#name' => $table,
+        "#type" => "submit",
+        "#value" => $this->t("Add year"),
+        "#name" => $table,
         "#submit" => ["::addRow"],
-        '#attributes' => [
-          'class' => ['add-row-btn'],
+        "#ajax" => [
+          "wrapper" => "form-wrapper",
         ],
       ];
 
       $form["table-$table"] = [
-        '#type' => 'table',
-        '#header' => $table_headers,
+        "#type" => "table",
+        "#header" => $table_headers,
       ];
 
       for ($i = $this->rows[$table]; $i > 0; $i--) {
         foreach ($table_headers as $header) {
-          if ($header == 'Year') {
+          if ($header == "Year") {
             $form["table-$table"][$i]["$header"] = [
               "#type" => "number",
               "#disabled" => TRUE,
-              '#default_value' => date('Y') - $i + 1,
+              "#default_value" => date("Y") - $i + 1,
             ];
           }
-          elseif (in_array($header, ['Q1', 'Q2', 'Q3', 'Q4', 'YTD'])) {
+          elseif (in_array($header, ["Q1", "Q2", "Q3", "Q4", "YTD"])) {
             $form["table-$table"][$i]["$header"] = [
               "#type" => "number",
               "#disabled" => TRUE,
@@ -94,23 +96,31 @@ class TableForm extends FormBase {
           }
           else {
             $form["table-$table"][$i]["$header"] = [
-              '#type' => 'number',
+              "#type" => "number",
             ];
           }
         }
       }
     }
 
-    $form['add-table'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Add table'),
-      '#submit' => ['::addTable'],
+    $form["add-table"] = [
+      "#type" => "submit",
+      "#value" => $this->t("Add table"),
+      "#submit" => ["::addTable"],
+      "#ajax" => [
+        "wrapper" => "form-wrapper",
+      ],
     ];
 
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
-      '#name' => 'submit',
+    $form["actions"]["submit"] = [
+      "#type" => "submit",
+      "#value" => $this->t("Submit"),
+      "#name" => "submit",
+      "#ajax" => [
+        "event" => "click",
+        "callback" => "::updateFormAjax",
+        "wrapper" => "form-wrapper",
+      ],
     ];
 
     $form["#attached"]["library"][] = "module4/module4";
@@ -121,8 +131,11 @@ class TableForm extends FormBase {
    * Callback function for add new table.
    */
   public function addTable(array &$form, FormStateInterface $form_state) {
+    // Plus one table.
     $this->countTable++;
+    // Set default row count to new table.
     $this->rows[] = 1;
+    // Rebuild form.
     $form_state->setRebuild();
   }
 
@@ -130,8 +143,11 @@ class TableForm extends FormBase {
    * Callback function for add row button.
    */
   public function addRow(array &$form, FormStateInterface $form_state) {
-    $i = $form_state->getTriggeringElement()['#name'];
+    // Get table id to add row.
+    $i = $form_state->getTriggeringElement()["#name"];
+    // Add one row.
     $this->rows[$i]++;
+    // Rebuild form.
     $form_state->setRebuild();
   }
 
@@ -139,29 +155,51 @@ class TableForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($i = $form_state->getTriggeringElement()['#name'] !== 'submit') {
+
+    // Check submit.
+    if ($form_state->getTriggeringElement()["#name"] !== "submit") {
       return;
     }
 
+    // Get all values from tables.
     $values = $form_state->getValues();
 
+    $smallestTable = array_search(min($this->rows), $this->rows);
+
+    // Loop by count tables.
     for ($t = 0; $t < $this->countTable; $t++) {
       $isValue = FALSE;
       $isEmpty = FALSE;
+
+      // Loop by rows in table.
       for ($r = $this->rows[$t]; $r > 0; $r--) {
+
+        // Loop by table headres.
         foreach ($values["table-$t"][$r] as $head => $val) {
-          if (in_array($head, ['Year', 'Q1', 'Q2', 'Q3', 'Q4', 'YTD'])) {
+          if (in_array($head, ["Year", "Q1", "Q2", "Q3", "Q4", "YTD"])) {
             continue;
           }
-          if (!$isValue && !$isEmpty && $val !== '') {
-            $isValue = TRUE;
+          if ($r <= $this->rows[$smallestTable]) {
+            if (!$isValue && !$isEmpty && $val !== "") {
+              $isValue = TRUE;
+            }
+            if ($isValue && !$isEmpty && $val == "") {
+              $isEmpty = TRUE;
+            }
+            if (!$isValue && $isEmpty && $val !== "") {
+              $form_state->setErrorByName("Invalid", $this->t("Invalid"));
+            }
+            if ($isValue && $isEmpty && $val !== "") {
+              $form_state->setErrorByName("Invalid", $this->t("Invalid"));
+            }
+
+            if ($values["table-$smallestTable"]["$r"][$head] == "" && $val !== "" ||
+              $values["table-$smallestTable"]["$r"][$head] !== "" && $val == "") {
+              $form_state->setErrorByName("Invalid", $this->t("Invalid"));
+            }
           }
-          if ($isValue && !$isEmpty && $val == '') {
-            $isEmpty = TRUE;
-            $isValue = FALSE;
-          }
-          if (!$isValue && $isEmpty && $val !== '') {
-            $form_state->setErrorByName('Invalid', 'Invalid');
+          else {
+            $form_state->setErrorByName("Invalid", $this->t("Invalid"));
           }
         }
       }
@@ -172,6 +210,49 @@ class TableForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    for ($t = 0; $t < $this->countTable; $t++) {
+      for ($r = $this->rows[$t]; $r > 0; $r--) {
+        $q1 = $q2 = $q3 = $q4 = 0;
+        $val = $form_state->getValue(["table-$t", $r]);
+        if ($val["Jan"] != "" || $val["Feb"] != "" || $val["Mar"] != "") {
+          $q1 = round(((int) $val["Jan"] + (int) $val["Feb"] + (int) $val["Mar"] + 1) / 3, 2);
+        }
+        if ($val["Apr"] != "" || $val["May"] != "" || $val["Jun"] != "") {
+          $q2 = round(((int) $val["Apr"] + (int) $val["May"] + (int) $val["Jun"] + 1) / 3, 2);
+        }
+        if ($val["Jul"] != "" || $val["Aug"] != "" || $val["Sep"] != "") {
+          $q3 = round(((int) $val["Jul"] + (int) $val["Aug"] + (int) $val["Sep"] + 1) / 3, 2);
+        }
+        if ($val["Oct"] != "" || $val["Nov"] != "" || $val["Dec"] != "") {
+          $q4 = round(((int) $val["Oct"] + (int) $val["Nov"] + (int) $val["Dec"] + 1) / 3, 2);
+        }
+
+        $form["table-$t"]["$r"]["Q1"]["#value"] = $q1;
+        $form["table-$t"]["$r"]["Q2"]["#value"] = $q2;
+        $form["table-$t"]["$r"]["Q3"]["#value"] = $q3;
+        $form["table-$t"]["$r"]["Q4"]["#value"] = $q4;
+
+        $ytd = round(($q1 + $q2 + $q3 + $q4 + 1) / 4, 2);
+        $form["table-$t"]["$r"]["YTD"]["#value"] = $ytd;
+      }
+    }
+
+    $this->messenger()->addStatus("Valid");
+  }
+
+  /**
+   * Callback function to update form.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Return form to update.
+   */
+  public function updateFormAjax(array $form, FormStateInterface $form_state) {
+    return $form;
   }
 
 }
